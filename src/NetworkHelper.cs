7f291f9f9915f732643160c6e7e8a913f6c995f6
twostars <i.am.twostars@gmail.com>
2018-06-24 23:39:49 +1000

    Forward to IP of default connected adapter. Using the DNS method didn't guarantee any degree of accuracy as to which address to use. Checking the interfaces directly gets us the first applicable registered adapter, which then gets us the IP it's actually using.

--- src/NetworkHelper.cs
+++ src/NetworkHelper.cs
@@ -1,12 +1,14 @@
 ﻿using System.Net;
+using System.Net.NetworkInformation;
 
 namespace KOUPnPMapper
 {
     public static class NetworkHelper
     {
         private static string _LocalIPAddress = string.Empty;
+
         /// <summary>
-        /// This returns a String representation of the Current Machines IP Address
+        /// This returns a String representation of the current machine's IP Address
         /// </summary>
         public static string LocalIPAddress
         {
@@ -14,23 +16,35 @@ namespace KOUPnPMapper
             {
                 if (string.IsNullOrEmpty(_LocalIPAddress))
                 {
-                    IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
-                    if (host.AddressList.Length > 0)
-                    {
-                        foreach (var addr in host.AddressList)
-                        {
-                            // Discard those that aren't IPv4
-                            if (addr.AddressFamily != System.Net.Sockets.AddressFamily.InterNetwork)
-                                continue;
-
-                            _LocalIPAddress = addr.ToString();
-                            break;
-                        }
-                    }
+                    var localIP = GetDefaultLocalIP();
+                    if (localIP != null)
+                        _LocalIPAddress = localIP.ToString();
                 }
+
                 return _LocalIPAddress;
             }
         }
 
+        public static IPAddress GetDefaultLocalIP()
+        {
+            var cards = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var card in cards)
+            {
+                if (card.OperationalStatus != OperationalStatus.Up)
+                    continue;
+
+                var props = card.GetIPProperties();
+                if (props == null)
+                    continue;
+
+                foreach (var ip in props.UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        return ip.Address;
+                }
+            }
+
+            return null;
+        }
     }
 }
